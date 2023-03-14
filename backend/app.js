eee058b9912259ed9379930f407bb6c725f1895c
AnneClarin <amtclarin@gmail.com>
2023-03-14 14:39:39 -0600

    Implement Express Server Security

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+const csurf = require('csurf');
+
 const usersRouter = require('./routes/api/users');
 const transactionsRouter = require('./routes/api/transactions');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -12,7 +17,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
 app.use('/api/users', usersRouter);
 app.use('/api/transactions', transactionsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
