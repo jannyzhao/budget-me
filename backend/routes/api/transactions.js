5e3806548d2abf6394b6f62300bdec752c74e2b8
AnneClarin <amtclarin@gmail.com>
2023-03-14 20:29:11 -0600

    create user model

--- backend/routes/api/transactions.js
+++ backend/routes/api/transactions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
+/* GET transactions listing. */
 router.get('/', function(req, res, next) {
   res.json({
     message: "GET /api/transactions"
