130ecf1528f244fe12bc907cb551450555e6655a
Janny Zhao <zhao.janny@gmail.com>
2023-03-16 14:25:36 -0700

    Configure NavBar using Bootstrap

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import "./NavBar.css";
 import { logout } from "../../store/session";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/esm/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 function NavBar() {
   const loggedIn = useSelector((state) => {
@@ -19,12 +24,42 @@ function NavBar() {
   const getLinks = () => {
     if (loggedIn) {
       return (
-        <div className="links-nav">
-          <Link to={"/transactions"}>All Transactions</Link>
-          <Link to={"/profile"}>Profile</Link>
-          <Link to={"/transactions/new"}>Add a new transaction</Link>
-          <button onClick={logoutUser}>Logout</button>
-        </div>
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">BudgetMe</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href={"/"}>Home</Nav.Link>
+                <Nav.Link href={"/transactions"}>All Transactions</Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href={"/profile"}>Profile</NavDropdown.Item>
+                  <NavDropdown.Item href={"/transactions/new"}>
+                    Add New Transaction
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={logoutUser}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search Transaction"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="primary">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       );
     } else {
       return (
@@ -35,10 +70,10 @@ function NavBar() {
       );
     }
   };
-
+  
   return (
     <>
-      <h1>Budget Me</h1>
+      {/* <h1>Budget Me</h1> */}
       {getLinks()}
     </>
   );
