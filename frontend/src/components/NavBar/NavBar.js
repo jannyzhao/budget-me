315301aff25fdc0a24cd757cc3bfe19330671344
Janny Zhao <81343916+jannyzhao@users.noreply.github.com>
2023-03-17 12:29:15 -0700

    Update frontend/src/components/NavBar/NavBar.js
    Co-authored-by: Jan Clarin <jervinct@gmail.com>

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -74,7 +74,7 @@ function NavBar() {
               >
                 <Nav.Link href={"/"}>Home</Nav.Link>
                 <Nav.Link href={"/login"}>Login</Nav.Link>
-                <Nav.Link href={"/signup"}>Signup</Nav.Link>
+                <Nav.Link href={"/signup"}>Sign up</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
