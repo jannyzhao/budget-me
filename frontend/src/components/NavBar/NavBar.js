c9f67b7be233adebb37a4d21ae18d8ddc6179d9e
Janny Zhao <81343916+jannyzhao@users.noreply.github.com>
2023-03-17 12:30:02 -0700

    Configure NavBar using Bootstrap (#10)
    * Configure NavBar using Bootstrap
    
    * Update with LoggedIn navbar and LoggedOut navbar
    
    * Update frontend/src/components/NavBar/NavBar.js
    
    Co-authored-by: Jan Clarin <jervinct@gmail.com>
    
    * Update frontend/src/components/NavBar/NavBar.js
    
    Co-authored-by: Jan Clarin <jervinct@gmail.com>
    
    * Update frontend/src/components/NavBar/NavBar.js
    
    Co-authored-by: Jan Clarin <jervinct@gmail.com>
    
    ---------
    
    Co-authored-by: Jan Clarin <jervinct@gmail.com>

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import "./NavBar.css";
 import { logout } from "../../store/session";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function NavBar() {
   const loggedIn = useSelector((state) => {
@@ -19,26 +23,68 @@ function NavBar() {
   const getLinks = () => {
     if (loggedIn) {
       return (
-        <div className="links-nav">
-          <Link to={"/transactions"}>All Transactions</Link>
-          <Link to={"/profile"}>Profile</Link>
-          <Link to={"/transactions/new"}>Add a new transaction</Link>
-          <button onClick={logoutUser}>Logout</button>
-        </div>
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href={"/"}>BudgetMe</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href={"/"}>Home</Nav.Link>
+                <Nav.Link href={"/transactions"}>All Transactions</Nav.Link>
+                <NavDropdown title="Menu" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href={"/profile"}>Profile</NavDropdown.Item>
+                  <NavDropdown.Item href={"/transactions/new"}>
+                    Add New Transaction
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={logoutUser}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search Transaction"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="primary">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       );
     } else {
       return (
-        <div className="links-auth">
-          <Link to={"/signup"}>Signup</Link>
-          <Link to={"/login"}>Login</Link>
-        </div>
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href={"/"}>BudgetMe</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="d-flex"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href={"/"}>Home</Nav.Link>
+                <Nav.Link href={"/login"}>Log in</Nav.Link>
+                <Nav.Link href={"/signup"}>Sign up</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       );
     }
   };
 
   return (
     <>
-      <h1>Budget Me</h1>
       {getLinks()}
     </>
   );
