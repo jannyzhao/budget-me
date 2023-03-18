0d4592835aaf968d68f628834c804eeb2bbff6ee
Janny Zhao <81343916+jannyzhao@users.noreply.github.com>
2023-03-17 23:50:20 -0700

    Create Amount Spent card (#15)
    * Add Amount Spent card in MainPage
    
    * Update ReadMe
    
    * Fix spacing between Monthly Income and Amount Spent

--- frontend/src/components/AmountSpent/AmountSpent.js
+++ frontend/src/components/AmountSpent/AmountSpent.js
@@ -2,7 +2,7 @@ import Card from "react-bootstrap/Card";
 import React from "react";
 import Container from "react-bootstrap/esm/Container";
 
-function MonthlyIncome({ className }) {
+function AmountSpent({ className }) {
   return (
     <div className={className}>
       <Card
@@ -10,22 +10,22 @@ function MonthlyIncome({ className }) {
         border="secondary"
         className="text-center"
       >
-        <Card.Header as="h6">Monthly Income</Card.Header>
+        <Card.Header as="h6">Amount Spent</Card.Header>
 
         <Card.Body>
           <Card.Img
             variant="top"
-            src="calendar.png"
+            src="money.png"
             style={{ width: "100px", height: "auto" }}
           />
           <Card.Subtitle className="mb-2 text-muted">
-            Income for March
+            Money Spent in March
           </Card.Subtitle>
-          <Card.Text>$4500.00</Card.Text>
+          <Card.Text>$180.00</Card.Text>
         </Card.Body>
       </Card>
     </div>
   );
 }
 
-export default MonthlyIncome;
+export default AmountSpent;
--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -2,11 +2,15 @@ import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import MonthlyIncome from "../MonthlyIncome/MonthlyIncome";
 import TransactionTable from "../TransactionTable/TransactionTable";
+import AmountSpent from "../AmountSpent/AmountSpent";
 
 function MainPage() {
   return (
-    <Container>
-      <MonthlyIncome className="mb-4" />
+    <Container className="pt-4">
+      <div className="d-flex gap-4">
+        <MonthlyIncome className="pr-4" />
+        <AmountSpent className="mb-4" />
+      </div>
       <TransactionTable />
     </Container>
   );
