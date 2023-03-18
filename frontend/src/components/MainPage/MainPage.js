807abd717b137cad777b5ce55796b44232f44d71
Janny Zhao <zhao.janny@gmail.com>
2023-03-17 23:14:43 -0700

    Add Amount Spent card in MainPage

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import MonthlyIncome from "../MonthlyIncome/MonthlyIncome";
 import TransactionTable from "../TransactionTable/TransactionTable";
+import AmountSpent from "../AmountSpent/AmountSpent";
 
 function MainPage() {
   return (
     <Container>
       <MonthlyIncome className="mb-4" />
+      <AmountSpent className="mb-4" />
       <TransactionTable />
     </Container>
   );
