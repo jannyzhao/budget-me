e5fa120b93ec59b9a83ab095ddc7b3a854388283
Janny Zhao <81343916+jannyzhao@users.noreply.github.com>
2023-03-18 19:11:22 -0700

    Add Balance Card (#17)

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -3,13 +3,15 @@ import Container from "react-bootstrap/esm/Container";
 import MonthlyIncome from "../MonthlyIncome/MonthlyIncome";
 import TransactionTable from "../TransactionTable/TransactionTable";
 import AmountSpent from "../AmountSpent/AmountSpent";
+import Balance from "../Balance/Balance";
 
 function MainPage() {
   return (
     <Container className="pt-4">
-      <div className="d-flex gap-4">
+      <div className="d-flex justify-content-center gap-4">
         <MonthlyIncome className="pr-4" />
         <AmountSpent className="mb-4" />
+        <Balance className="mb-4" />
       </div>
       <TransactionTable />
     </Container>
