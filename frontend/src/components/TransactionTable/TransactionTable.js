42caeae718af14d899a1a6ce303c696eb091846a
AnneClarin <amtclarin@gmail.com>
2023-03-18 23:28:27 -0600

    start transaction rendering

--- frontend/src/components/TransactionTable/TransactionTable.js
+++ frontend/src/components/TransactionTable/TransactionTable.js
@@ -1,50 +1,63 @@
 import Table from "react-bootstrap/Table";
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import TransactionRow from "./TransactionRow";
 import AddTransaction from "./AddTransaction";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchUserTransactions,
+  clearTransactionErrors,
+} from "../../store/transactions";
 
-
-function TransactionTable() {
-
-  return (
-    <Container>
-      <Table responsive striped bordered hover>
-        <thead>
-          <tr>
-            <th>DATE</th>
-            <th>COMPANY</th>
-            <th>DESCRIPTION</th>
-            <th>TYPE</th>
-            <th>CATEGORY</th>
-            <th>AMOUNT</th>
-          </tr>
-        </thead>
-        <tbody>
-          <TransactionRow
-            date="03/15/2023"
-            company="NIKE"
-            description="Shoes"
-            amount="$150.00"
-          />
-          <TransactionRow
-            date="03/17/2023"
-            company="DOORDASH"
-            description="Marufuku"
-            amount="$30.00"
-          />
-          <TransactionRow
-            date="03/19/2023"
-            company="META"
-            description="Direct Deposit"
-            amount="$4000.00"
-          />
-        </tbody>
-      </Table>
-      < AddTransaction />
-    </Container>
+function TransactionTable({ transactions }) {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.session.user);
+  const userTransactions = useSelector((state) =>
+    Object.values(state.transactions.user)
   );
+
+  useEffect(() => {
+    dispatch(fetchUserTransactions(currentUser._id));
+    return () => dispatch(clearTransactionErrors());
+  }, [currentUser, dispatch]);
+
+  if (userTransactions.length === 0) {
+    return (
+      <Container>
+        <div>{currentUser.username} has no Transactions</div>
+        <AddTransaction />
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <Table responsive striped bordered hover>
+          <thead>
+            <tr>
+              <th>DATE</th>
+              <th>COMPANY</th>
+              <th>DESCRIPTION</th>
+              <th>TYPE</th>
+              <th>CATEGORY</th>
+              <th>AMOUNT</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+              <TransactionRow
+                date={transaction.date}
+                amount={transaction.amount}
+                company={transaction.company}
+                description={transaction.description}
+                type={transaction.type}
+                category={transaction.category}
+              />
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    );
+  }
 }
 
 export default TransactionTable;
